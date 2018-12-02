7d2a0dc874704cd764f53a66b451a2119e9a5b75
Michael Andrés Murcia Contreras <mamurciac@unal.edu.co>
2018-12-02 00:50:50 -0500

    App Init

---
+++ Videocall/app/src/main/java/agora/io/videocall/VideochatActivity.java
@@ -0,0 +1,13 @@
+package agora.io.videocall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideochatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videochat);
+    }
+}
